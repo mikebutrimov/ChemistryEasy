35b39e065696c2f7644548d299bf23ec3108841d
Mike Butrimov <mike.butrimov@gmail.com>
2017-07-09 13:50:59 +0300

    add proto interface of chem element

---
+++ app/src/main/java/org/unhack/chemistryeasy/elements/IChemElement.java
@@ -0,0 +1,63 @@
+package org.unhack.chemistryeasy.elements;
+
+/**
+ * Interface for Chemistry Element
+ *
+ */
+
+public interface IChemElement {
+    //standart getters
+
+    /**
+     *
+     * @return element name as String
+     */
+    String getElementName();
+
+    /**
+     *
+     * @return element number as int in periodic table
+     */
+    int getElementNumber();
+
+    /**
+     *
+     * @return atomic mass as float
+     */
+    float getAtomicMass();
+
+    /**
+     *
+     * @return element's block as String (S,P,D,F)
+     */
+    String getBlockName();
+
+    //Some othe getters shoul be there, these ones ar as part of an example.
+    //
+
+
+    //standart setters
+    //base params are setted by class constructor or fabric
+
+    /**
+     *
+     * @param mutator - a Chemistry Element Mutator class instance that describes
+     *                spesific permutations for an element. Like reaction on temperatures
+     *                or pressure changes. This part is an example. Need to do some research
+     *                on best architecture
+     *
+     */
+    void setMutator(ChemElementMutator mutator);
+
+    //statefull methods
+
+    /**
+     * This method is used to invoke mutation on element
+     *
+     * @return True if mutation was a success, otherwise false
+     *
+     */
+    boolean doMutation();
+
+
+}
